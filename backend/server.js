99e175f50462116178c8a3adde7eb6e9db189b8f
irythill <h.sp97@hotmail.com>
2024-12-06 16:14:29 -0300

    still tweaking

--- backend/server.js
+++ backend/server.js
@@ -14,10 +14,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
+    await mongoose.connect(process.env.MONGO_URI)
     console.log('MongoDB connected!')
   } catch (err) {
     console.error(err.message)
