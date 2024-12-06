acfecd105920dfda078604a5586320dfc9ee2f33
irythill <h.sp97@hotmail.com>
2024-12-06 14:51:09 -0300

    added modal button to confirm if user wants to delete artist

--- frontend/src/components/BandList.jsx
+++ frontend/src/components/BandList.jsx
@@ -1,35 +1,55 @@
-import { Link } from 'react-router-dom'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons'
-import '../css/BandList.css'
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
+import ConfirmationModal from './ConfirmationModal';
+import '../css/BandList.css';
 
 const BandList = () => {
-  const [bands, setBands] = useState([])
-  
-  const API_URL = 'http://localhost:5000'
+  const [bands, setBands] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [bandToDelete, setBandToDelete] = useState(null);
+
+  const API_URL = 'http://localhost:5000';
 
   useEffect(() => {
     axios.get(`${API_URL}/bands`)
-    .then(response => {
-      setBands(response.data)
-    })
-    .catch(error => {
-      console.log('Error fetching the bands!', error)
-    })
-  }, [])
+      .then(response => {
+        setBands(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching the bands!', error);
+      });
+  }, []);
+
+  const handleDeleteClick = (band) => {
+    setBandToDelete(band);
+    setIsModalOpen(true);
+  };
+
+  const handleConfirmDelete = () => {
+    axios.delete(`${API_URL}/bands/${bandToDelete._id}`)
+      .then(() => {
+        setBands(bands.filter(band => band._id !== bandToDelete._id));
+        setIsModalOpen(false);
+      })
+      .catch(error => {
+        console.error('Error deleting the band!', error);
+      });
+  };
+
+  const handleCancelDelete = () => {
+    setIsModalOpen(false);
+    setBandToDelete(null);
+  };
 
   return (
-    <div className='band-list-container'>
+    <div className="band-list-container">
       <div className="band-list">
         {bands.map(band => (
-          <div key={band._id} className='band-card'>
-            <img 
-            src={`${API_URL}/${band.image}`} 
-            alt={band.name} 
-            className='band-image'
-            />
+          <div key={band._id} className="band-card">
+            <img src={`${API_URL}/${band.image}`} alt={band.name} className="band-image" />
             <div className="band-info">
               <h3>{band.name}</h3>
               <p>{band.genre}</p>
@@ -37,18 +57,23 @@ const BandList = () => {
               <p>{band.description}</p>
               <div className="band-actions">
                 <Link to={`/edit-band/${band._id}`}>
-                  <FontAwesomeIcon icon={faEdit} />
+                  <FontAwesomeIcon icon={faEdit} className="edit-icon" />
                 </Link>
-                <Link to={`/delete-band/${band._id}`}>
-                  <FontAwesomeIcon icon={faTrash} />
-                </Link>
+                <button onClick={() => handleDeleteClick(band)} className="delete-button">
+                  <FontAwesomeIcon icon={faTrash} className="delete-icon" />
+                </button>
               </div>
             </div>
           </div>
         ))}
       </div>
+      <ConfirmationModal
+        isOpen={isModalOpen}
+        onRequestClose={handleCancelDelete}
+        onConfirm={handleConfirmDelete}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default BandList
+export default BandList;
